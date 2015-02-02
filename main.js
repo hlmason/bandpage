64260f22b498bf42d823287dfb10033ce2e48440
Heidi Mason <heidi@musiclaps.com>
2015-02-01 17:46:36 -0800

    Cross domain request working

--- main.js
+++ main.js
@@ -1,45 +1,39 @@
-$(document).ready(function() 
-{	
-	$('form').submit(function(event) 
-	{
-		event.preventDefault();
+function requestCrossDomain(site, callback) {
 
-		var $searchField = $('#search');
-		var $submitButton = $('#submit');
-		var $blogPostsContainer = $('#blog-posts-container');
-		$searchField.prop("disabled", true); // Disables search field until search is finished
-		$submitButton.attr("disabled", true).val("Searching..."); // Disables search button and displays message until search is finished
-	
-		// blogPostsContainer.html('');
+	if(!site) {
+		alert('Please enter a blog url');
+		$('#search').css('border', '2px solid orange');
+		return false;
+	}
+
+	// var yql = 'https://query.yahooapis.com/v1/public/yql?q=' + encodeURIComponent('select * from html where url="' + site + '"') + '&format=xml&callback=?';
+
+	var yql = 'https://query.yahooapis.com/v1/public/yql?q=' + encodeURIComponent('SELECT title, link, description FROM rss where url="' + site + '"') + '&format=xml&callback=?';
+
+	// $.getJSON(yql, function(data) {
+	// 	if(data.results[0]) {
+	// 		data = data.results[0].replace(/<script[^>]*>[\s\S]*?<\/script>/);
+
+	// 		if(typeof callback == 'function') {
+	// 			callback(data);
+	// 		}
+	// 	}
 		
-		var blogAPI = "http://www.mytime.com/api/v1/deals.json?what=Massage&when=Anytime&where=34.052200,-118.242800?callback=?";
-		var blogURL = $searchField.val(); // Get user's search word or phrase
-		var blogOptions = 
-		{
-			format: "json"
-		};
-		function displayBlogPosts(data) 
-		{
-			var blogPostsHTML = '';
-			if(data.items.length > 0) 
-			{
-				$.each(data.items, function(i, blogPost) 
-				{
-					blogPostsHTML += '<div class="blog-post-1">';
-					blogPostsHTML += '<h4>' + blogPost.title + '</h4>';
-					blogPostsHTML += '<p>' + blogPost.description + '</p>';
-					blogPostsHTML += '<a href="' + blogPost.link + '" target="blank"></a>';
-					blogPostsHTML += '</div>';
-				});
-			} else 
-			{
-				blogPostsHTML = "<p>No blog posts found</p>"
-			}
-			blogPostsContainer.html(blogPostsHTML);
-		    $searchField.prop("disabled", false); // Reactivates search field once search is finished
-      		$submitButton.attr("disabled", false).val("Search"); // Reactivates search button once search is finished
-		}
-		$.getJSON(blogAPI, blogOptions, displayBlogPosts);
+	// 	else throw new Error('Nothing returned from getJSON.');
+	// });
+
+	$.getJSON(yql, function(results) {
+		console.log(results);
+		// var blogPostHTML = '';
+		// results = JSON.parse(results);
+		$.each(results.query.results.item, function(i, blogPost) {
+			console.log(i + ' ' + blogPost);
+			// blogPostHTML += '<div class="blog-post">';
+			// blogPostHTML += '<h4>' + blogPost.title + '</h4>';
+			// blogPostHTML += '<div class="center"><a href="' + blogPost.link + '" target="blank"></a></div>';
+			// blogPostHTML += '<p>' + blogPost.description + '</p>';
+			// $('#blog-posts-container').html(blogPostHTML);
+		});
 	});
 
-});
+}
